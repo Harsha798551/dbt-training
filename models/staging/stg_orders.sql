f17b3f0922adee3747386396241a0bc88646c77f
konakallaharsha74@gmail.com <konakallaharsha74@gmail.com>
2022-11-18 18:05:34 +0000

    developing stage orders

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,15 @@
+select 
+--from raw orders 
+o.orderid, o.orderdate,o.shipdate, o.shipmode, o.ordercostprice, o.ordersellingprice,
+o.ordersellingprice - o.ordercostprice  as orderprofit,
+-- from raw customer
+c.customername, c.segment, c.country,
+-- from raw_products
+p.category, p.productname, p.subcategory
+from {{ ref('raw_orders') }} as o
+left join 
+{{ ref('raw_customers') }} as c
+on o.customerid = c.customerid 
+left join 
+{{ ref('raw_products') }} as p
+on o.productid=p.productid
