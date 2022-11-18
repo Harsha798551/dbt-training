920ab81f0244be3171f00fa9988ee1f75dfa6498
konakallaharsha74@gmail.com <konakallaharsha74@gmail.com>
2022-11-18 18:25:56 +0000

    Developed report profit by customer and report profit  by product

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -3,9 +3,9 @@ select
 o.orderid, o.orderdate,o.shipdate, o.shipmode, o.ordercostprice, o.ordersellingprice,
 o.ordersellingprice - o.ordercostprice  as orderprofit,
 -- from raw customer
-c.customername, c.segment, c.country,
+c.customername, c.segment, c.country,c.customerid,
 -- from raw_products
-p.category, p.productname, p.subcategory
+p.category, p.productname, p.subcategory, p.productid
 from {{ ref('raw_orders') }} as o
 left join 
 {{ ref('raw_customers') }} as c
